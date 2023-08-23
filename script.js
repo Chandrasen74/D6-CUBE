3bd254d63da83f601c2de53747e95b1a4bbaed01
Chandrasen74 <107239606+Chandrasen74@users.noreply.github.com>
2023-08-23 11:29:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Check out my blog:
+https://mackfitz.hashnode.dev/laying-out-dots-on-a-dice-using-displayflex-pseudo-classes
+or
+https://dev.to/mackfitz/laying-out-dots-on-a-dice-using-displayflex-pseudo-classes-5444
+for a tutorial on how to make dice faces display one, two, three, four, five and six dots - as a dice would, but using display:flex instead of grid like I did here
+*/
